bb761446937f1dcbe85cad8d7591bee19469d205
Luis Andrés Arce C <luilliarcec@gmail.com>
2020-02-06 13:27:49 -0500

    Update Services Provider Bug Fixed Validator

--- src/EcuadorIdentificationServiceProvider.php
+++ src/EcuadorIdentificationServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace Luilliarcec\LaravelEcuadorIdentification;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 use Luilliarcec\LaravelEcuadorIdentification\Support\EcuadorIdentification;
+use Luilliarcec\LaravelEcuadorIdentification\Validations\EcuadorIdentificationValidator;
 
 class EcuadorIdentificationServiceProvider extends ServiceProvider
 {
@@ -14,9 +16,13 @@ class EcuadorIdentificationServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('laravel-ecuador-identification.php'),
+                __DIR__ . '/../config/config.php' => config_path('laravel-ecuador-identification.php'),
             ], 'config');
         }
+
+        Validator::resolver(function ($translator, $data, $rules, $messages, $customAttributes) {
+            return new EcuadorIdentificationValidator($translator, $data, $rules, $messages, $customAttributes);
+        });
     }
 
     /**
@@ -25,7 +31,7 @@ class EcuadorIdentificationServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'laravel-ecuador-identification');
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'laravel-ecuador-identification');
 
         // Register the main class to use with the facade
         $this->app->singleton('EcuadorIdentification', function () {
