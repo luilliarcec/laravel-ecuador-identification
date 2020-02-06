25025c2520257d1c51fa3bfc153cdb9b1b665ec4
Luis Andrés Arce C <luilliarcec@gmail.com>
2020-02-06 11:23:16 -0500

    Adding Validator

--- src/EcuadorIdentificationServiceProvider.php
+++ src/EcuadorIdentificationServiceProvider.php
@@ -12,36 +12,10 @@ class EcuadorIdentificationServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'laravel-ecuador-identification');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-ecuador-identification');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('laravel-ecuador-identification.php'),
             ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-ecuador-identification'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/laravel-ecuador-identification'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/laravel-ecuador-identification'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
         }
     }
 
---
+++ src/Validations/EcuadorIdentificationValidator.php
@@ -0,0 +1,92 @@
+<?php
+
+
+namespace Luilliarcec\LaravelEcuadorIdentification\Validations;
+
+use Illuminate\Validation\Validator;
+use Luilliarcec\LaravelEcuadorIdentification\Exceptions\EcuadorIdentificationException;
+use Luilliarcec\LaravelEcuadorIdentification\Support\EcuadorIdentification;
+
+class EcuadorIdentificationValidator extends Validator
+{
+    /**
+     * Determine if the value is valid
+     *
+     * @var bool
+     */
+    private $isValid = false;
+
+    /**
+     * Validation types
+     *
+     * @var array
+     */
+    private $types = [
+        'personal_identification' => 'validatePersonalIdentification',
+        'natural_ruc' => 'validateNaturalPersonRuc',
+        'public_ruc' => 'validatePublicCompanyRuc',
+        'private_ruc' => 'validatePrivateCompanyRuc',
+        'is_juridical_person' => 'validateIsJuridicalPersons',
+        'is_natural_person' => 'validateIsNaturalPersons',
+    ];
+
+    /**
+     * Validate that the Ecuadorian identification is valid
+     *
+     * @param $attribute
+     * @param $value
+     * @param $parameters
+     * @return bool
+     * @throws EcuadorIdentificationException
+     */
+    public function validateEcuadorIdentification($attribute, $value, $parameters)
+    {
+        $validator = new EcuadorIdentification();
+
+        $lowerRule = explode(':', strtolower($this->currentRule))[0];
+
+        try {
+            $this->isValid = $validator->{$this->types[$parameters[0]]}($value) == null ? false : true;
+        } catch (\Exception $exception) {
+            throw new EcuadorIdentificationException("Custom validation rule {$lowerRule}:{$parameters[0]} does not exist");
+        }
+
+        if (!$this->isValid) {
+            $this->setCustomMessages([$lowerRule => $this->getMessageEcuador($attribute, $lowerRule)]);
+            return $this->isValid;
+        }
+
+        return $this->isValid;
+    }
+
+    /**
+     * Get the translated message or the default message.
+     *
+     * @param $attribute
+     * @param $rule
+     * @return mixed|string
+     */
+    protected function getMessageEcuador($attribute, $rule)
+    {
+        $key = 'validation.' . $rule;
+
+        return $this->getTranslator()->get($key) != $key ?
+            $this->getTranslator()->get($key) :
+            "The {$this->getAttributeEcuador($attribute)} field does not have the corresponding country format. (Ecuador)";
+    }
+
+    /**
+     * Get the translated attribute or the default attribute.
+     *
+     * @param $attribute
+     * @return mixed
+     */
+    protected function getAttributeEcuador($attribute)
+    {
+        $key = 'validation.attributes.' . $attribute;
+
+        return $this->getTranslator()->get($key) != $key ?
+            $this->getTranslator()->get($key) :
+            $attribute;
+    }
+}
